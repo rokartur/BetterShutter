2e9b9426d7125ed660e63d8cf47eef3f3ec192f7
rokartur <rokartur@icloud.com>
2026-05-26 21:16:03 +0200

    Initial Commit

---
+++ BetterCap/BetterCapApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterCapApp.swift
+//  BetterCap
+//
+//  Created by artur on 26/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct BetterCapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
